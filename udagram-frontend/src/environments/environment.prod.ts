49cc94c41b086edae1a959a06803aa220bc5d2cc
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2022-03-09 02:43:37 +0200

    Updating Project Files & set environments

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://ammarmassoududagramapienv1-env.eba-3byezuiq.us-east-1.elasticbeanstalk.com/",
 };
 
 /*
